f691b14425bb1bdd2fb3a941a19f4c4beed09747
kelvin <denlinato@gmail.com>
2023-03-19 17:42:49 +0000

    testing cron job

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const moment =  require("moment") ; 
-
+const cron =  require("node-cron");
 var day = moment();
 
 const nextMonth =  day.clone().add(2, "minute") ; 
@@ -8,4 +8,11 @@ const daysBetween  =  nextMonth.diff(day);
 const dur = moment.duration(daysBetween);
 console.log(nextMonth)
 console.log(daysBetween);
-console.log(dur.asMinutes())
+console.log(dur.asSeconds())
+
+
+console.log(cron.validate("* * * * * *"))
+
+cron.schedule("* * * * * *", function () {
+  console.log("hello");
+});
