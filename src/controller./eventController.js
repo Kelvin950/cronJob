f856a282f2230d17b9061e2ea79b043a307b939c
kelvin <denlinato@gmail.com>
2023-03-19 16:02:42 +0000

    creating events

---
+++ src/controller./eventController.js
@@ -0,0 +1,6 @@
+const User = require("../models/user"); 
+const Event =  require("../models/event");
+
+exports.createEvent =  async()=>{
+
+}
--- src/controller./userController.js
+++ src/controller./userController.js
@@ -3,3 +3,33 @@ const User  =  require("../models/user") ;
 
 
 
+exports.createUser =  async(req, res)=>{
+ 
+     
+    const  {name , email} =  req.body ; 
+
+
+    const EmailAvailable = await findOne({email}) ; 
+    
+    if(!EmailAvailable){
+    throw new Error("Email is not available"); 
+
+    } 
+     
+
+    const user =  new  User({
+        email , name  
+    }) ; 
+
+    await user.save() ; 
+
+
+    res.send({
+        user 
+    })
+
+
+}
+
+
+
